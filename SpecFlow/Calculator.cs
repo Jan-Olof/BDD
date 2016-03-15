a83dc9d3a05fc6349d241ca5a1ec008ca50ce322
Jan-Olof Nilsson <janolof@gmail.com>
2016-03-15 20:00:07 +0100

    SpecFlow

---
+++ SpecFlow/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace SpecFlow
+{
+    public class Calculator
+    {
+        public int FirstNumber { set; private get; }
+
+        public int SecondNumber { set; private get; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
